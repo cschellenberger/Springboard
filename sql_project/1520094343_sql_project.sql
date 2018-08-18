40ff710cfe075763bf17990043d7d320e229f47c
Cody Schellenberger <cody@schellenberger.me>
2018-08-18 07:04:54 -0600

    Committing Assignment

--- sql_project/1520094343_sql_project.sql
+++ sql_project/1520094343_sql_project.sql
@@ -1,28 +1,6 @@
-/* Welcome to the SQL mini project. For this project, you will use
-Springboard' online SQL platform, which you can log into through the
-following link:
-
-https://sql.springboard.com/
+/* https://sql.springboard.com/
 Username: student
-Password: learn_sql@springboard
-
-The data you need is in the "country_club" database. This database
-contains 3 tables:
-    i) the "Bookings" table,
-    ii) the "Facilities" table, and
-    iii) the "Members" table.
-
-Note that, if you need to, you can also download these tables locally.
-
-In the mini project, you'll be asked a series of questions. You can
-solve them using the platform, but for the final deliverable,
-paste the code for each solution into this script, and upload it
-to your GitHub.
-
-Before starting with the questions, feel free to take your time,
-exploring the data, and getting acquainted with the 3 tables. */
-
-
+Password: learn_sql@springboard */
 
 /* Q1: Some of the facilities charge a fee to members, but some do not.
 Please list the names of the facilities that do. */
@@ -74,7 +52,7 @@ who signed up. Do not use the LIMIT clause for your solution. */
 
 SELECT firstname,
        surname
-FROM Members
+ FROM Members
 WHERE joindate = (SELECT MAX(joindate) FROM Members)
 
 /* Q7: How can you produce a list of all members who have used a tennis court?
@@ -82,11 +60,16 @@ Include in your output the name of the court, and the name of the member
 formatted as a single column. Ensure no duplicate data, and order by
 the member name. */
 
-SELECT facility.name AS facility,
-       member.firstname + ' ' + member.surname AS name
-FROM Facilities facility
-JOIN Bookings book
-  ON book.facid = facility.facid
+SELECT f.name AS facility,
+       CONCAT(m.firstname, ' ', m.surname) AS name
+FROM Bookings b
+    JOIN Facilities f
+      ON f.facid = b.facid
+    JOIN Members m 
+      ON m.memid = b.memid
+WHERE f.facid IN (0,1)
+GROUP BY 1,2
+ORDER BY 2
 
 /* Q8: How can you produce a list of bookings on the day of 2012-09-14 which
 will cost the member (or guest) more than $30? Remember that guests have
@@ -95,10 +78,50 @@ the guest user's ID is always 0. Include in your output the name of the
 facility, the name of the member formatted as a single column, and the cost.
 Order by descending cost, and do not use any subqueries. */
 
+SELECT f.name AS facility,
+       CONCAT(m.firstname, ' ', m.surname) AS name,
+       CASE WHEN b.memid = '0' THEN (b.slots * f.guestcost)
+            ELSE (b.slots * f.membercost) END AS cost
+FROM Bookings b
+    JOIN Facilities f
+      ON f.facid = b.facid
+    JOIN Members m 
+      ON m.memid = b.memid
+WHERE b.starttime LIKE '2012-09-14%'
+  AND ((m.memid =0 AND b.slots * f.guestcost > 30)
+	OR (m.memid !=0 AND b.slots * f.membercost > 30))
+ORDER BY 3 DESC
 
 /* Q9: This time, produce the same result as in Q8, but using a subquery. */
 
+SELECT member, facility, cost
+FROM (
+
+SELECT CONCAT(m.surname, ', ', m.firstname) AS member, 
+       f.name AS facility, 
+       CASE WHEN m.memid =0 THEN b.slots * f.guestcost
+            ELSE b.slots * f.membercost
+            END AS cost
+      FROM `Members` m
+      JOIN `Bookings` b 
+        ON m.memid = b.memid
+INNER JOIN `Facilities` f 
+        ON b.facid = f.facid
+WHERE b.starttime LIKE '2012-09-14%') AS bookings
+	
+WHERE cost > 30
+ORDER BY cost DESC
 
 /* Q10: Produce a list of facilities with a total revenue less than 1000.
 The output of facility name and total revenue, sorted by revenue. Remember
 that there's a different cost for guests and members! */
+
+SELECT f.name AS facility,
+       SUM(CASE WHEN b.memid = '0' THEN (b.slots * f.guestcost)
+            ELSE (b.slots * f.membercost) END) AS revenue
+FROM Bookings b
+    JOIN Facilities f
+      ON f.facid = b.facid
+GROUP BY 1
+HAVING revenue < 1000
+ORDER BY 2 DESC
